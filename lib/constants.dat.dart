974dd05402c7d90a1a42e3f6fac537cc5460d155
TAOUTI Mohammed Madani <taoutimohammedmadani@gmail.com>
2021-06-30 18:00:43 +0100

    commit 01

---
+++ lib/constants.dat.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color KMainColor = Colors.green;
+const Color KTextColor = Colors.white;
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:tasks_list/widgets/tasks_list.dart';
 
+import '../constants.dat.dart';
+
 class TasksScreen extends StatelessWidget {
   const TasksScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
+      backgroundColor: KMainColor,
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        backgroundColor: Colors.lightBlueAccent,
+        backgroundColor: KMainColor,
         child: Icon(Icons.add),
       ),
       body: Column(
@@ -26,10 +28,10 @@ class TasksScreen extends StatelessWidget {
                   child: Icon(
                     Icons.list,
                     size: 30.0,
-                    color: Colors.lightBlueAccent,
+                    color: KMainColor,
                   ),
                   radius: 30.0,
-                  backgroundColor: Colors.white,
+                  backgroundColor: KTextColor,
                 ),
                 SizedBox(height: 10.0),
                 Text(
@@ -37,11 +39,11 @@ class TasksScreen extends StatelessWidget {
                   style: TextStyle(
                       fontSize: 50.0,
                       fontWeight: FontWeight.w700,
-                      color: Colors.white),
+                      color: KTextColor),
                 ),
                 Text(
                   '12 Tasks',
-                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  style: TextStyle(color: KTextColor, fontSize: 18.0),
                 ),
               ],
             ),
@@ -53,7 +55,7 @@ class TasksScreen extends StatelessWidget {
                 borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(20.0),
                     topRight: Radius.circular(20.0)),
-                color: Colors.white,
+                color: KTextColor,
               ),
               child: TasksList(),
             ),
